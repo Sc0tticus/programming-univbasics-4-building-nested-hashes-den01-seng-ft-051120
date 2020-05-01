39c4205321c85d7b76624478a0eeda92d6ae367e
Scott Singer <ssinger303@gmail.com>
2020-05-01 19:06:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,11 +14,10 @@ def monopoly_with_second_tier
   # complexity.
  hash ={
     :railroads => {
-      
-    },
-    :pieces => 4,
+      :pieces => 4,
     :rent_in_dollars => {},
     :names => {}
+    },
   }
 hash
 end
